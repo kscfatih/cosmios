3f74ccee4028dba739ae66b0edaa711e93da2e7e
Fatih Kuşçu <fatihkusci252@gmail.com>
2025-01-05 12:07:47 +0300

    comiit

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login', user_login, name='login')
+    path('login', user_login, name='login'),
+    path('logout', user_logout, name='logout'),
+    path('register', register, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            print('login işlemi başarılı')
+            login(request, user)
+            messages.success(request, 'Login işleminiz başarılı')
+            return redirect('blog_liste')
+        else:
+            messages.error(request, 'Kullanıcı adı veya şifre hatalı')
+            return render(request, 'accounts/login.html')
+    
     return render(request, 'accounts/login.html')
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'Başarıyla çıkış yapıldı!')
+    return redirect('login')
+
+def register(request):
+    return render(request, 'accounts/register.html')
