46afd12a7a78da319959f38e2d5d3daf5f4981ef
Fatih Kuşçu <fatihkusci252@gmail.com>
2025-01-12 12:38:27 +0300

    api detail category

--- blogapp/api/urls.py
+++ blogapp/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('categories', category_view, name='api-categories')
+    path('categories', category_view, name='api-categories'),
+    path('categories/<int:pk>', category_detail_view, name='api-detail-categories')
 ]
--- blogapp/api/views.py
+++ blogapp/api/views.py
@@ -17,3 +17,26 @@ def category_view(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','PUT','DELETE'])
+def category_detail_view(request, pk):
+    try:
+        category = Category.objects.get(id=pk)
+    except:
+        return Response('İlgili id ile bir obje bulunamdı',status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)      
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
